5b8e8bdf8928b41e888218b1fc62a66616f2df89
Eric Roos <ericroos13@gmail.com>
2016-11-21 11:39:33 -0600

    add forgot password, and update names

--- widget.js
+++ widget.js
@@ -9,19 +9,26 @@ BookshoutLoginWidget.generate_form = function(selector){
   var emailElement = document.createElement("input");
   emailElement.setAttribute("type","text");
   emailElement.setAttribute("placeholder", "email");
+  emailElement.setAttribute("name", "user[email]");
 
   var passwordElement = document.createElement("input");
   passwordElement.setAttribute("type", "password");
   passwordElement.setAttribute("placeholder", "password");
+  passwordElement.setAttribute("name", "user[password]");
 
   var submitElement = document.createElement("input");
   submitElement.setAttribute("type", "submit");
   submitElement.setAttribute("value", "Login");
 
 
+  var forgotPasswordLink = document.createElement("a");
+  forgotPasswordLink.setAttribute("href", "https://www.bookshout.com/web/password");
+  forgotPasswordLink.appendChild(document.createTextNode("Forgot Password?"));
+
   formElement.appendChild(emailElement);
   formElement.appendChild(passwordElement);
   formElement.appendChild(submitElement);
+  formElement.appendChild(forgotPasswordLink);
 
   container.appendChild(formElement);
 };
